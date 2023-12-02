31a034fb68b0135b8c7afe4872b442a4dcb5da1b
Ian Frye <ian_frye@outlook.com>
2023-12-02 11:54:02 -0500

    adding day 2 christmas challenge

--- solo-project-movie-search/fetchMovie.js
+++ solo-project-movie-search/fetchMovie.js
@@ -1,7 +1,8 @@
 const API_KEY = '419e0663';
 const OPEN_MOVIE_BASE_URL = 'http://www.omdbapi.com/';
 
-export async function getMovieArray(title) {
+//Function for getting the results of all the movies that match that title and extracting their id's to pass to the getMovieDetails Array Function
+async function getMovieArray(title) {
   const url = `${OPEN_MOVIE_BASE_URL}?apikey=${API_KEY}&s=${title}`;
   try {
     const response = await fetch(url);
@@ -9,18 +10,33 @@ export async function getMovieArray(title) {
       throw new Error(`Error fetching Data ${response.statusText}`);
     }
     const data = await response.json();
-    const movieIDArray = await data.Search.map((movie) => {
-      return movie.imdbID;
-    });
-    return movieIDArray;
+
+    //if movie doesn't exist in database - database will return data with a Response = 'False" if this happens then return null to the next function and return null to the parent funtion to display error message in HTML
+    if (data.Response === 'False') {
+      return null;
+    } else {
+      //if movies are found - loop through them and extract their IMDB Ids and put them in an array to be passed to the next funciton so that it can call to the datbase and get the details from each movie to display in the DOM
+      const movieIDArray = await data.Search.map((movie) => {
+        return movie.imdbID;
+      });
+
+      //Return the movie ID Array
+      return movieIDArray;
+    }
   } catch (error) {
     console.error(error);
   }
 }
 
 export async function getMovieDetailsArray(title) {
+  //Assign the movie array from the function above into a constant to be used below
   const movieIDArray = await getMovieArray(title);
 
+  //if the movieIDArray is null or there was a problem and it was undefined - return Null to the parent function to display error message in the DOM
+  if (!movieIDArray) {
+    return null;
+  }
+  //if the movieIDArray is valid - loop through it and make a network call for each movie getting that movie's object and storing it in an array to pass to the parent function index.js
   const movieDetailsArray = await Promise.all(
     movieIDArray.map(async (movieID) => {
       const response = await fetch(
@@ -30,5 +46,7 @@ export async function getMovieDetailsArray(title) {
       return data;
     })
   );
+
+  //return this function to parent.
   return movieDetailsArray;
 }
--- solo-project-movie-search/index.js
+++ solo-project-movie-search/index.js
@@ -1,20 +1,114 @@
 import { getMovieDetailsArray } from './fetchMovie.js';
-
-const searchResultsContainer = document.querySelector('.search-results');
+//HTML elements from file
+const searchResultsContainer = document.querySelector(
+  '#search-results-container'
+);
+const watchListContainer = document.querySelector('#watch-list-container');
 const movieString = document.querySelector('#search-box');
-const searchButton = document.querySelector('#search-button');
 const form = document.querySelector('#search-form');
+
+//desclare the two arrays to keep track of the data changes
+let watchListArray = [];
+let movieDetailsArray = [];
 
 //event listeners
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   if (movieString.value) {
-    const movieIDArray = await getMovieDetailsArray(movieString.value);
-    renderCards(movieIDArray);
+    movieDetailsArray = await getMovieDetailsArray(movieString.value);
+    renderCards(movieDetailsArray);
   }
 });
-document.addEventListener('click', (e) => {});
 
-function renderCards(movieIDArray) {
-  searchResultsContainer.innerHTML = {};
+document.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.dataset.id) {
+    const movieID = target.dataset.id;
+    const movie = movieDetailsArray.find((movie) => movie.imdbID === movieID);
+
+    if (movie) {
+      if (isInWatchList(movieID)) {
+        removeFromWatchList(movieID);
+      } else {
+        addToWatchList(movie);
+      }
+      toggleButtonText(target);
+    }
+  }
+});
+
+//utility functions
+
+function isInWatchList(movieId) {
+  return watchListArray.some((movie) => movie.imdbID === movieId);
+}
+
+function addToWatchList(movie) {
+  watchListArray.push(movie);
+}
+
+function removeFromWatchList(movieId) {
+  watchListArray = watchListArray.filter((movie) => movie.imdbID !== movieId);
+}
+
+function toggleButtonText(button) {
+  button.innerHTML = isInWatchList(button.dataset.id)
+    ? '<i class="fa-solid fa-circle-minus"></i> Remove'
+    : '<i class="fa-solid fa-circle-plus"></i> Watchlist';
+}
+
+//Render Function -  This function takes the returned Data from the fetchMovie.js function and renders a card for each movie and displays it on to the DOM - if there was an error it will handle the error by displaying an error message on the DOM
+function renderCards(movieDetailsArray) {
+  movieString.value = ``;
+  searchResultsContainer.innerHTML = ``;
+
+  //error message displayed on DOM
+  if (!movieDetailsArray) {
+    searchResultsContainer.innerHTML = `
+    <h3 class="error-message">
+    Unable to find what you’re looking for. Please try another search.
+    </h3>`;
+    return;
+  }
+
+  //Creating movie card for each movie in the array
+  movieDetailsArray.map((movie) => {
+    searchResultsContainer.innerHTML += `
+    <div class="search-result-card">
+            <div class="img-container">
+              <img src="${
+                movie.Poster.length < 5
+                  ? './images/icons/movie-film-icon.svg'
+                  : movie.Poster
+              }" alt="${movie.Title} Poster" />
+            </div>
+            <div class="text-container">
+              <div class="top-section">
+                <h3 class="movie-title">${movie.Title}</h3>
+                <span id="movie-rating"
+                  > ${
+                    movie.Ratings.length > 0
+                      ? '<i class="fa-solid fa-star"></i> ' +
+                        movie.Ratings[0].Value.slice(0, 3)
+                      : ''
+                  }</span
+                >
+              </div>
+              <div class="middle-section">
+                <span class="run-time">${movie.Runtime}</span
+                ><span class="genre">${movie.Genre}</span
+                ><button id="add-to-watch-list" data-id="${movie.imdbID}">
+                  <i class="fa-solid fa-circle-plus"></i> Watchlist
+                </button>
+              </div>
+              <div class="bottom-section">
+                <p class="movie-description">
+                 ${movie.Plot}
+                </p>
+              </div>
+            </div>
+          </div>
+    `;
+  });
 }
