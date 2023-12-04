d11f893fd983e429308d1aa8130ca1718d03d24e
Ian Frye <ifrye@theresource.com>
2023-12-04 09:27:09 -0500

    updated hacker news reducer to use local storage

--- hacker-news-clone/components/Story.js
+++ hacker-news-clone/components/Story.js
@@ -2,7 +2,7 @@ export default function Story(story) {
   return `
     <div class="story">
       <div> 
-        <span class="gray">${story.index || ""}</span>
+        <span class="gray">${story.index || ''}</span>
         <span class="upvote">▲</span>
         <a href="${story.url}">${story.title}</a>
         <span>(${story.domain})</span>
@@ -16,8 +16,8 @@ export default function Story(story) {
           </a>
           |
           <span class="favorite" data-story='${JSON.stringify(story)}'>
-            <img class="heart" src="https://icon.now.sh/heart/ccc">
-            ${story.isFavorite ? "Remove From Favorites" : "Add To Favorites"}
+            <i class="fa-solid fa-heart"></i>
+            ${story.isFavorite ? 'Remove From Favorites' : 'Add To Favorites'}
           </span>
         </div>
       </div>
--- hacker-news-clone/store.js
+++ hacker-news-clone/store.js
@@ -1,38 +1,45 @@
 function createStore(reducer) {
-   let currentState = reducer(undefined, {});
-   
-   return {
-     getState: () => currentState,
-     dispatch: action => {
-        currentState = reducer(currentState, action);    
-     } 
-   }
+  const storedFavorites = localStorage.getItem('favorites');
+  const initialState = storedFavorites
+    ? { favorites: JSON.parse(storedFavorites).favorites }
+    : reducer(undefined, {});
+
+  let currentState = initialState;
+
+  const store = {
+    getState: () => currentState,
+    dispatch: (action) => {
+      currentState = reducer(currentState, action);
+    },
+  };
+
+  return store;
 }
 
 const initialState = {
-  favorites: []  
-}
+  favorites: [],
+};
 
 function favoritesReducer(state = initialState, action) {
-   switch (action.type) {
-      case "ADD_FAVORITE": {
-        const addedFavorite = action.payload.favorite;
-        const favorites = [...state.favorites, addedFavorite];
-        return { favorites };
-      }
-      case "REMOVE_FAVORITE": {
-        const removedFavorite = action.payload.favorite;
-        const favorites = state.favorites.filter(favorite => favorite.id !== removedFavorite.id);
-        return { favorites };
-      }
-      default:
-        return state;
-   } 
+  switch (action.type) {
+    case 'ADD_FAVORITE': {
+      const addedFavorite = action.payload.favorite;
+      const favorites = [...state.favorites, addedFavorite];
+      localStorage.setItem('favorites', JSON.stringify({ favorites }));
+      return { favorites };
+    }
+    case 'REMOVE_FAVORITE': {
+      const removedFavorite = action.payload.favorite;
+      const favorites = state.favorites.filter(
+        (favorite) => favorite.id !== removedFavorite.id
+      );
+      localStorage.setItem('favorites', JSON.stringify({ favorites }));
+      return { favorites };
+    }
+    default:
+      return state;
+  }
 }
 
-// const action = { type: "ADD_FAVORITE", payload: { favorite: { title: "story1", id: 1 } } };
-
 const store = createStore(favoritesReducer);
-// store.dispatch(action);
-// console.log(store.getState());
 export default store;
