e502080bff8fb78b2d003cdae7b00dc1e2f98260
Ian Frye <ian_frye@outlook.com>
2023-12-14 04:53:59 -0500

    Adding Javascriptmas Day 14

--- React-Projects/meme-generator/fetchMemes.js
+++
@@ -1,8 +0,0 @@
-export default async function fetchMemes() {
-  const response = await fetch('https://api.imgflip.com/get_memes');
-  if (!response.ok) {
-    throw new Error(`Error fetching Data ${response.status || ''}`);
-  }
-  const data = await response.json();
-  return data;
-}
